fb850aac21e09c2ae4989767540f12e4f488dc57
Suneel kumar <58180985+l04660@users.noreply.github.com>
2020-10-08 12:49:47 -0500

    files added

--- static/js/5.7730518b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpSCHNEIDER-MVP"]=this["webpackJsonpSCHNEIDER-MVP"]||[]).push([[5],{251:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(41),i=(e(17),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.g)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o.m)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
-//# sourceMappingURL=5.7730518b.chunk.js.map
--- static/js/5.c0e07c8a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpSCHNEIDER-MVP"]=this["webpackJsonpSCHNEIDER-MVP"]||[]).push([[5],{244:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(38),i=(e(13),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.g)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o.m)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
-//# sourceMappingURL=5.c0e07c8a.chunk.js.map
